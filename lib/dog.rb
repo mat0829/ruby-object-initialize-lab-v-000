b4d7556e3f398e58bd901dfde69fe89f8f3a346d
David Anderson <matandersonlv@hotmail.com>
2018-09-19 23:12:18 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,9 @@
+# Create a new class called "Dog"
+class Dog
+
+def initialize(name, breed = "Mutt")
+  @name = name
+  @breed = breed
+end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,8 @@
+# Create a new class called "Person"
+class Person
+
+def initialize(name)
+  @name = name
+end
+
+end
