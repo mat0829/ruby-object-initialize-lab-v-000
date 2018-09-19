13d077b442b18d12feb97fdbf163bf3cd409891a
David Anderson <matandersonlv@hotmail.com>
2018-09-19 23:01:00 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,24 +1,12 @@
 # Create a new class called "Dog"
 class Dog
-  def initialize(name)
+  def initialize(name, breed)
     @name = name
-  end
-
-
-  def name
-    @name
-  end
-
-  def initialize(breed)
     @breed = "Mutt"
   end
 
-  def name=(breed)
-    @breed = breed
-  end
-
-  def breed
-    @breed
+  def name
+    @name
   end
 
 end
