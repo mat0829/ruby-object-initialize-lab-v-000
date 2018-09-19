cac564d99dcec86ff9b9efca954103a54c406797
David Anderson <matandersonlv@hotmail.com>
2018-09-19 22:42:38 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,16 +4,12 @@ class Dog
     @name = name
   end
 
+  def name=(name)
+    @name = name
+  end
+
   def name
     @name
-  end
-
-  def initialize(breed)
-    @breed = "Mutt"
-  end
-
-  def breed
-    @breed
   end
 
 end
