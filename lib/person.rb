32b7801618b7595a2a118064332192aeef72ec00
David Anderson <matandersonlv@hotmail.com>
2018-09-19 23:08:18 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,8 @@
 # Create a new class called "Person"
 class Person
-  def initialize(name)
-    @name = name
-  end
 
-  def name=(name)
-    @name = name
-  end
-
-  def name
-    @name
-  end
+def initialize(name)
+  @name = name
+end
 
 end
